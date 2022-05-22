41de0a6097b4cdac075225201776ab92cdade65e
Mauro Bense <maurobense@Mauros-MacBook-Pro.local>
2022-05-22 00:51:02 -0300

    Login preFinal

--- GameFinderApp/code.js
+++ GameFinderApp/code.js
@@ -10,3 +10,43 @@ document.getElementById('eye').onclick = function(){
         icon.innerText = "visibility"
     }
 }
+
+let index = 1;
+let body = document.querySelector('#body');
+let bodyStyle = window.getComputedStyle(body);
+let bodyBackground = bodyStyle.getPropertyValue('background');
+let slider = document.querySelectorAll('.slides');
+//background: url(img/carousel.png)#C4C4C4;
+
+document.getElementById('right').onclick = function(){
+    if(index == 6){
+        index = 0;
+    }
+    index++;
+    let slideSelect = document.querySelector(`#slide_${index}`);
+    body.style.background = `url(img/carousel${index}.png)#C4C4C4`; 
+    body.style.backgroundSize = 'cover';
+    body.style.backgroundRepeat = 'no-repeat';
+    for(let i = 0; i < slider.length;i++){
+        slider[i].style.background = '#5151513f';
+    }
+    slideSelect.style.background = '#515151';
+    
+}
+
+document.getElementById('left').onclick = function(){
+    if(index == 1){
+        index = 7;
+    }
+    index--;
+    let slideSelect = document.querySelector(`#slide_${index}`);
+    body.style.background = `url(img/carousel${index}.png)#C4C4C4`; 
+    body.style.backgroundSize = 'cover';
+    body.style.backgroundRepeat = 'no-repeat';
+    for(let i = 0; i < slider.length;i++){
+        slider[i].style.background = '#5151513f';
+    }
+    slideSelect.style.background = '#515151';
+
+    
+}
